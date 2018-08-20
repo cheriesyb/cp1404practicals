c000550c2b9cf501d49e6586dbfa969fbe1b1ec8
chezz281 <cherie.barringhaus@my.jcu.edu.au>
2018-08-20 16:47:56 +1000

    "Set into functions"

--- prac_02/broken_program.py
+++ prac_02/broken_program.py
@@ -5,12 +5,21 @@ Broken program to determine score status
 
 # TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+
+def main():
+    score = float(input("Enter score: "))
+    print(determine_status(score))
+
+
+def determine_status(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
