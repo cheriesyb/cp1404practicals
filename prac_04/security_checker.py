9b7d462bb29efd01946db4c970297f8b7053a5ec
cheriesyb <Dragfire281>
2018-08-21 15:48:59 +1000

    "Add if/else conditions: check if input username is in authorised users list"

--- prac_04/security_checker.py
+++ prac_04/security_checker.py
@@ -1,3 +1,10 @@
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
              'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
              'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+get_username = input("Username: ")
+
+if get_username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
