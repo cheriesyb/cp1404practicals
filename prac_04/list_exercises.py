848233e2824423d85585fb31cdb3d948ed0a4db7
cheriesyb <Dragfire281>
2018-08-25 12:04:48 +1000

    "Complete the average numbers and change index for the last number"

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,15 +1,17 @@
 numbers = []
 
 def main():
+    total = 0
     for number in range(5):
-        value = input("Number: ")
+        value = int(input("Number: "))
         numbers.append(value)
+        total += 1
 
     print("The first number is {}".format(numbers[0]))
-    print("The last number is {}".format(numbers[4]))
+    print("The last number is {}".format(numbers[-1]))
     print("The smallest number is {}".format(min(numbers)))
-    print("The largest number is {}".format(max(numbers)))
-    print("The average of the numbers is {}".format()) #find the average
+    print("The largest number is {}".format(max(numbers))
+    print("The average of the numbers is {}".format(sum(numbers)/total))
 
 main()
 
