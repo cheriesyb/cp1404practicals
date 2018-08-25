435c4d4f5456d4c2693c879570eca64e8d323940
cheriesyb <Dragfire281>
2018-08-25 11:56:46 +1000

    "Refactor into its own method using extract"

--- prac_04/cuumulative_totals.py
+++ prac_04/cuumulative_totals.py
@@ -12,16 +12,28 @@ def main():
     for month in range(1, number_of_months + 1): #for loop
         income = float(input("Enter income for month {}: ".format(month))) #change to string format()
         incomes.append(income) #add values to list
+    display_income_report(number_of_months, incomes)
 
+
+def display_income_report(number_of_months, incomes):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1): # start counter at 1
+    for month in range(1, number_of_months + 1):  # start counter at 1
         income = incomes[month - 1]
-        total += income #add each variable one after the other to be cumulative
+        total += income  # add each variable one after the other to be cumulative
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
 
 main()
 
-#retest; still works as it should
-#how to refactor? cannot refactor using selected elements
+
+"""
+loop through the number of months and append each income to an empty list.
+count each input starting from 1
+The number of loops depend on the input. So we should use a for loop
+Sum each income through the loop
+"""
+
+"""
+refactor months to number_of_months
+"""
