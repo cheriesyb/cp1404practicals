260f15bf7d861065b5963516cac1c396278cc902
cheriesyb <Dragfire281>
2018-09-04 15:24:41 +1000

    "Add name field Add __str__ method to class"

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,13 +4,17 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
-
+        name: string, reference name for car
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self.odometer = 0
+
+    def __str__(self):
+        return "{}, fuel={}, odometer={}".format(self.name, self.fuel, self.odometer)  # fuel? fuel-distance
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
