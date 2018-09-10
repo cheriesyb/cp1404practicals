add55da42f68811e6a9ee312aaa9adb2713d07f1
cheriesyb <Dragfire281>
2018-09-11 09:42:14 +1000

    "Test the last two methods: get age and is vintage"

---
+++ prac_06/guita_test.py
@@ -0,0 +1,16 @@
+from prac_06.guitar import Guitar
+
+CURRENT_YEAR = 2018
+
+def testing():
+    name = "Gibson L-5 CES"
+    year = 1922
+    cost = 16035.40
+
+    guitar = Guitar(name, year, cost)
+    other = Guitar("Another guitar", 2012, 1512.9)
+
+    print("{} get_age() - Expected {}. Got {}".format(guitar.name, 96, guitar.get_age()))
+    print("{} get_age() - Expected {}. Got {}".format(other.name, 6, other.get_age()))
+
+testing()
