21972f1627bbd624fed5fbe3c89a1476dbc3fb06
cheriesyb <Dragfire281>
2018-09-11 14:54:25 +1000

    "Create a main Loop input until blank Add guitar to empty list"

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,9 +1,17 @@
 from prac_06.guitar import Guitar
 
-guitars = []
+def main():
+    guitars = []
 
-print("My guitars!")
-name = input("Name: ")
-year = int(input("Year: "))
-cost = int(input("Cost: "))
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        add_guitar = Guitar(name, year, cost)
+        guitars.append(add_guitar)
+        print(add_guitar, "added.")
+        name = input("Name: ")
 
+
+main()
